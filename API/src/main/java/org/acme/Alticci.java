d380547f355ae0c8482059c88d0be7e596ca8fd3
SlayBelleNoxian <33154114+SlayBelleNoxian@users.noreply.github.com>
2022-11-11 14:25:20 +0000

    Changing name of variable

--- API/src/main/java/org/acme/Alticci.java
+++ API/src/main/java/org/acme/Alticci.java
@@ -2,17 +2,16 @@ package org.acme;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import java.math.*;
 
 
 @Path("/alticci")
 public class Alticci {
     Functions function = new Functions();
     @GET
-    @Path("/{id}")
-    public String alticci(String id) {
+    @Path("/{n}")
+    public String alticci(String n) {
         //Transform the String into a integer
-        long value = Long.parseLong(id);
+        long value = Long.parseLong(n);
         //The numbers can't be negative so the loop doesn't go infinite
         if (value < 0) return "Numero tem de ser maior ou igual a 0";
         //returning the function "Alticci"
